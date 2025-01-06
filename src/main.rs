a28fbfce553e9b163354f55001f07092a4da59cd
Jaska <pusanteemu@gmail.com>
2025-01-06 17:06:53 +0200

    remove old code

--- src/main.rs
+++ src/main.rs
@@ -360,33 +360,3 @@ async fn stream_raw_logs(Path(devid): Path<String>, body: Body)  {
         file.write(&chunk).unwrap();
     }
 }
-
-#[axum::debug_handler]
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
