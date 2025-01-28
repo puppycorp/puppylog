7ce4d896f74dbb5c309a9583c2ad948d0d6ce998
Jaska <pusanteemu@gmail.com>
2025-01-28 09:17:38 +0200

    modify

--- examples/server/src/main.rs
+++ examples/server/src/main.rs
@@ -1,22 +1,18 @@
 use std::{thread::sleep, time::Duration};
-
-use log::Log;
 use puppylog::LoggerBuilder;
 
 fn main() {
     let logger = LoggerBuilder::new()
 		.stdout()
 		.server("http://localhost:3337/api/logs").unwrap()
-		//.server("https://heartbeat.pusatec.fi/api/logs").unwrap()
-		//.authorization("jyrki")
 		.build()
 		.unwrap();
 
-	// log::info!("Hello, world!");
-	// log::warn!("Warning!");
-	// log::error!("Error!");
-	// log::debug!("Debug!");
-	// log::trace!("Trace!");
+	log::info!("Hello, world!");
+	log::warn!("Warning!");
+	log::error!("Error!");
+	log::debug!("Debug!");
+	log::trace!("Trace!");
 
 	for i in 0..10 {
 		log::info!("Hello, world! {}", i);
