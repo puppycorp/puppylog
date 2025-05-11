188177e722ab875623eeae438b6679cc5197d95b
Jaska <pusanteemu@gmail.com>
2025-05-11 14:37:23 +0300

    update generated js too

--- assets/puppylog.js
+++ assets/puppylog.js
@@ -1000,7 +1000,7 @@ var settingsSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20
 var searchSvg = `<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="20px" height="20px"><path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"/></svg>`;
 var formatTimestamp2 = (ts) => {
   const date = new Date(ts);
-  return date.toLocaleString();
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")} ${String(date.getHours()).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}`;
 };
 var escapeHTML = (str) => {
   const div = document.createElement("div");
