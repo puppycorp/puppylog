a6ffb79d1de224d4e2166039d4ff717d754e9bf2
Jaska <pusanteemu@gmail.com>
2025-07-08 06:57:57 +0300

    feat: make navbar global

--- ts/app.ts
+++ ts/app.ts
@@ -6,6 +6,7 @@ import { routes } from "./router"
 import { segmentPage, segmentsPage } from "./segment-page"
 import { settingsPage } from "./settings"
 import { Container } from "./ui"
+import { Navbar } from "./navbar"
 import { queriesPage } from "./queries"
 
 window.onload = () => {
@@ -14,15 +15,36 @@ window.onload = () => {
 		throw new Error("No body element found")
 	}
 	const container = new Container(body)
+	const navbar = new Navbar()
+	container.add(navbar)
+	const pageRoot = document.createElement("div")
+	const pageContainer = new Container(pageRoot)
+	container.add(pageContainer)
+
+	const renderElem =
+		(handler: (root: HTMLElement, nav: Navbar) => any) => () => {
+			pageRoot.innerHTML = ""
+			navbar.setRight()
+			handler(pageRoot, navbar)
+		}
+	const renderContainer =
+		(handler: (root: Container, nav: Navbar) => any) => () => {
+			pageRoot.innerHTML = ""
+			navbar.setRight()
+			handler(pageContainer, navbar)
+		}
 	routes({
-		"/tests/logs": () => logtableTest(body),
-		"/settings": () => settingsPage(container),
-		"/devices": () => devicesPage(body),
-		"/segments": () => segmentsPage(container),
-		"/queries": () => queriesPage(body),
-		"/segment/:segmentId": (params: any) =>
-			segmentPage(body, params.segmentId),
-		"/pivot": () => PivotPage(body),
-		"/*": () => mainPage(body),
+		"/tests/logs": renderElem(logtableTest),
+		"/settings": renderContainer(settingsPage),
+		"/devices": renderElem(devicesPage),
+		"/segments": renderContainer(segmentsPage),
+		"/queries": renderElem(queriesPage),
+		"/segment/:segmentId": (params: any) => {
+			pageRoot.innerHTML = ""
+			navbar.setRight()
+			segmentPage(pageRoot, params.segmentId, navbar)
+		},
+		"/pivot": renderElem(PivotPage),
+		"/*": renderElem(mainPage),
 	})
 }
